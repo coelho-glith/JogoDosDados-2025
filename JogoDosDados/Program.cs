4f73eb03f3d6c140d65101f8233b23fc58e7c455
coelho-glith <coelho.soldado7@gmail.com>
2025-03-19 18:06:50 -0300

    Implementado eventos especiais de avanço e recuo de posição

--- JogoDosDados/Program.cs
+++ JogoDosDados/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace JogoDosDados.ConsoleApp
 {
-    /** Versão 2 - Controle de posição do jogador
-      * Armazenar a posição do jogador na pista e atualizar o valor após o lançamento do dado
-      * Exibir a posição atual do jogador na pista
-      * Definir a linha de chegada em 30 verificar se o jogador alcançou ou ultrapassou a linha de chegada
-      * Permitir o jogador realizar várias jogadas
+    /** Versão 3 - Eventos Especiais: Avanço extra e Recuo
+      * Implementar o evento de avanço extra nas posições 5, 10, 15, 25
+      * Implementar o evento de recuo nas posições 7, 13, 20
+      * Exibir mensagens informativas quando os eventos ocorrerem
+      * Atualizar a posição do jogador conforme as regras dos eventos 
+      * Exibir a nova posição
     **/
     internal class Program
     {
@@ -37,14 +38,33 @@
 
                     posicaoJogador += resultado;
 
+                    Console.WriteLine($"Você está na posição: {posicaoJogador} de {limiteLinhaChegada}!");
+
+                    if (posicaoJogador == 5 || posicaoJogador == 10 || posicaoJogador == 15 || posicaoJogador == 25)
+                    {
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("EVENTO ESPECIAL: Avanço extra de 3 casas!");
+
+                        posicaoJogador += 3;
+
+                        Console.WriteLine($"Você avançou para a posição: {posicaoJogador}!");
+                    }
+                    else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20)
+                    {
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("EVENTO ESPECIAL: Recuo de 2 casas!");
+
+                        posicaoJogador -= 2;
+
+                        Console.WriteLine($"Você recuou para a posição: {posicaoJogador}!");
+                    }
+
                     if (posicaoJogador >= limiteLinhaChegada)
                     {
                         jogoEmAndamento = false;
 
                         Console.WriteLine("Parabéns! Você alcançou a linha de chegada!");
-                    }
-                    else
-                        Console.WriteLine($"Você está na posição: {posicaoJogador} de {limiteLinhaChegada}!");
+                    }   
 
                     Console.WriteLine("----------------------------------");
                     Console.ReadLine();
